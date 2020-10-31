2e9a44a6eedc3c1066a0bdd07399bfdba5d07cba
ridwanullahgold <belloridwan60@gmail.com>
2020-10-31 08:08:18 +0100

    Prepared for deployment

--- server/config/index.js
+++ server/config/index.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 export default {
     databaseUrl:
-    process.env.DATABASE_URL || 'mongodb://localhost:27017/mevnmongo',
+    process.env.DATABASE_URL || 'mongodb://localhost:27017/sieben',
     url: process.env.APP_URL || 'http://localhost:3000',
     jwtSecret: process.env.JWT_SECRET || 'this-is-my-secret'
 
